9d77b406f2eb4e1060ee53cf55f166d18f8cf9bc
PavelNovik <brn2befree@gmail.com>
2022-12-05 20:36:45 +0300

    some changes

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,6 @@
   const App = window.App;
   const Truck = App.Truck;
   const DataStore = App.DataStore;
-  const myTruck = new Truck('ncc-1701', new DataStore());
+  const myTruck = new Truck('n3.14', new DataStore());
   window.myTruck = myTruck;
 })(window);
